c859fc7cffdd9132d3ba0e528ff90da6acf61b91
Yosef Hershberg <berioo2000@gmail.com>
2024-11-24 20:50:02 +0200

    changed share modal to be a parent of the trigger (more efficiant)

--- src/components/modals/ShareRecipeModal.tsx
+++ src/components/modals/ShareRecipeModal.tsx
@@ -16,104 +16,107 @@ import xLogo from '@/assets/logos/x-logo.png'
 
 const title = 'Check out this recipe I created with CulinaryGPT'
 
-type ShareRecipeModalProps = {
-    url: string,
-    isOpen: boolean,
-    close: () => void
-}
 
-const ShareRecipeModal: React.FC<ShareRecipeModalProps> = ({ url, isOpen, close }) => {
+const ShareRecipeModal: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
     const handleCopyToClipboard = async () => {
-        await navigator.clipboard.writeText(url)
+        await navigator.clipboard.writeText(window.location.href)
         toast({
             title: 'Link copied to clipboard',
             description: 'You can now share this link with your friends',
         })
-        close()
     }
 
     return (
-        <Dialog open={isOpen}>
-            {/* NOTE: Need this to prevent warning */}
-            <DialogTrigger></DialogTrigger>
+        <Dialog>
+            <DialogTrigger asChild>
+                {children}
+            </DialogTrigger>
 
             <DialogContent>
                 <DialogTitle>How Would you like to share your recipe?</DialogTitle>
 
-                {/* NOTE: Need this to prevent warning */}
-                <DialogDescription></DialogDescription>
+                <DialogDescription>
+                    Share this recipe with friends on social platforms
+                </DialogDescription>
 
                 <div className="flex justify-around">
-                    <Button
-                        variant='ghost'
-                        className="h-fit"
-                    >
-                        <a
-                            href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex flex-col items-center gap-2"
+                    <DialogClose asChild>
+                        <Button
+                            variant='ghost'
+                            className="h-fit"
                         >
-                            <img
-                                className="size-10"
-                                src={facebookLogo}
-                                alt="facebook logo"
-                            />
-                            <span>Facebook</span>
-                        </a>
-                    </Button>
+                            <a
+                                href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex flex-col items-center gap-2"
+                            >
+                                <img
+                                    className="size-10 object-contain"
+                                    src={facebookLogo}
+                                    alt="facebook logo"
+                                />
+                                <span>Facebook</span>
+                            </a>
+                        </Button>
+                    </DialogClose>
 
-                    <Button
-                        variant='ghost'
-                        className="h-fit"
-                    >
-                        <a
-                            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(title)}&url=${encodeURIComponent(url)}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex flex-col items-center gap-2"
+                    <DialogClose asChild>
+                        <Button
+                            variant='ghost'
+                            className="h-fit"
                         >
-                            <img
-                                className="size-10"
-                                src={xLogo}
-                                alt="x logo"
-                            />
-                            <span>X</span>
-                        </a>
-                    </Button>
+                            <a
+                                href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(title)}&url=${encodeURIComponent(window.location.href)}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex flex-col items-center gap-2"
+                            >
+                                <img
+                                    className="size-10 object-contain"
+                                    src={xLogo}
+                                    alt="x logo"
+                                />
+                                <span>X</span>
+                            </a>
+                        </Button>
+                    </DialogClose>
 
-                    <Button
-                        variant='ghost'
-                        className="h-fit"
-                    >
-                        <a
-                            href={`https://wa.me/?text=${encodeURIComponent(`${title} :: ${url}`)}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex flex-col items-center gap-2"
+                    <DialogClose asChild>
+                        <Button
+                            variant='ghost'
+                            className="h-fit"
                         >
-                            <img
-                                className="size-10"
-                                src={whatsappLogo}
-                                alt="whatsapp logo"
-                            />
-                            <span>WhatsApp</span>
-                        </a>
-                    </Button>
+                            <a
+                                href={`https://wa.me/?text=${encodeURIComponent(`${title} :: ${window.location.href}`)}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex flex-col items-center gap-2"
+                            >
+                                <img
+                                    className="size-10 object-contain"
+                                    src={whatsappLogo}
+                                    alt="whatsapp logo"
+                                />
+                                <span>WhatsApp</span>
+                            </a>
+                        </Button>
+                    </DialogClose>
 
-                    <Button
-                        variant='ghost'
-                        onClick={handleCopyToClipboard}
-                        className="flex flex-col items-center gap-2 h-fit"
-                    >
-                        <Copy className="size-10" />
-                        <span>Copy link</span>
-                    </Button>
+                    <DialogClose asChild>
+                        <Button
+                            variant='ghost'
+                            onClick={handleCopyToClipboard}
+                            className="flex flex-col items-center gap-2 h-fit"
+                        >
+                            <Copy className="size-10 object-contain" />
+                            <span>Copy link</span>
+                        </Button>
+                    </DialogClose>
                 </div>
 
                 <DialogClose
-                    onClick={close}
                     className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
                 >
                     <X className="h-4 w-4" />
--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from 'react'
+import { useLayoutEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Lottie from 'lottie-react'
 
@@ -18,73 +18,67 @@ type RecipePageProps = {
 const RecipePage: React.FC<RecipePageProps> = ({ createdRecipe, addToRecipesBtn }) => {
     const navigate = useNavigate()
 
-    const [isShareModalOpen, setIsShareModalOpen] = useState<boolean>(false)
-
     useLayoutEffect(() => {
         if (!createdRecipe) navigate('/create-new-recipe')
     }, []);
 
     if (createdRecipe) return (
-        <>
-            <ShareRecipeModal
-                url={window.location.href}
-                isOpen={isShareModalOpen}
-                close={() => setIsShareModalOpen(false)}
-            />
-            <main className='flex flex-col w-screen items-center bg-amber-100 dark:bg-zinc-700 pb-5'>
-                <div className='max-w-[50rem] flex flex-col items-center bg-orange/20 py-5 px-10 rounded-2xl m-5'>
+        <main className='flex flex-col w-screen items-center bg-amber-100 dark:bg-zinc-700 pb-5'>
+            <div className='max-w-[50rem] flex flex-col items-center bg-orange/20 py-5 px-10 rounded-2xl m-5'>
 
-                    <div className='flex flex-col items-center '>
-                        <h2 className='text-2xl font-semibold text-center'>{createdRecipe.recipe.title}</h2>
-                        <p className='text-lg text-center mt-4'>{createdRecipe.recipe.description}</p>
-                    </div>
-                    <div className='sm:size-[25rem] rounded-lg flex flex-col items-center mt-8 bg-transparent/10'>
-                        {createdRecipe.image_url ?
-                            <img
-                                src={createdRecipe.image_url}
-                                alt={createdRecipe.recipe.title}
-                                className='size-full object-cover rounded-lg aspect-square'
-                            /> :
-                            <Lottie animationData={loadingRecipeAnimation} className='size-full p-10' />
-                        }
-                    </div>
-                    {!createdRecipe.image_url &&
-                        <p className='text-lg text-center mt-4'>Loading image. One moment please...</p>
+                <div className='flex flex-col items-center '>
+                    <h2 className='text-2xl font-semibold text-center'>{createdRecipe.recipe.title}</h2>
+                    <p className='text-lg text-center mt-4'>{createdRecipe.recipe.description}</p>
+                </div>
+                <div className='sm:size-[25rem] rounded-lg flex flex-col items-center mt-8 bg-transparent/10'>
+                    {createdRecipe.image_url ?
+                        <img
+                            src={createdRecipe.image_url}
+                            alt={createdRecipe.recipe.title}
+                            className='size-full object-cover rounded-lg aspect-square'
+                        /> :
+                        <Lottie animationData={loadingRecipeAnimation} className='size-full p-10' />
                     }
-                    <div className='flex flex-col items-center mt-10'>
-                        <h3 className='text-2xl font-semibold'>Ingredients</h3>
-                        <ul className='list-disc mt-2 sm:grid grid-cols-2 gap-x-10'>
-                            {createdRecipe.recipe.ingredients.map(i => (
-                                <li className='mt-2' key={i.ingredient}>{i.ingredient}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className='flex flex-col items-center mt-10'>
-                        <h3 className='text-2xl font-semibold'>Instructions</h3>
-                        <ol className='list-decimal mt-2'>
-                            {createdRecipe.recipe.steps.map(step => (
-                                <li className='mt-2' key={step.step}>{step.step}</li>
-                            ))}
-                        </ol>
-                    </div>
-
                 </div>
-                <h1 className='font-bold sm:mb-0 mb-[4rem] sm:text-6xl text-5xl italic text-amber-800 dark:text-amber-600'>
-                    Bon Apetite!
-                </h1>
-                <div className='fixed w-full px-5 bottom-5 flex sm:justify-between justify-center gap-5'>
-                    {!addToRecipesBtn && <Button
-                        onClick={() => setIsShareModalOpen(true)}
-                        variant='secondary'
-                        className='min-w-[8rem] flex items-center gap-2 h-12 rounded-full px-5 hover:scale-105 transition duration-300 ease-in-out'
-                    >
-                        Share <Share2 className='size-4' />
-                    </Button>}
-                    {addToRecipesBtn}
+                {!createdRecipe.image_url &&
+                    <p className='text-lg text-center mt-4'>Loading image. One moment please...</p>
+                }
+                <div className='flex flex-col items-center mt-10'>
+                    <h3 className='text-2xl font-semibold'>Ingredients</h3>
+                    <ul className='list-disc mt-2 sm:grid grid-cols-2 gap-x-10'>
+                        {createdRecipe.recipe.ingredients.map(i => (
+                            <li className='mt-2' key={i.ingredient}>{i.ingredient}</li>
+                        ))}
+                    </ul>
                 </div>
-            </main>
+                <div className='flex flex-col items-center mt-10'>
+                    <h3 className='text-2xl font-semibold'>Instructions</h3>
+                    <ol className='list-decimal mt-2'>
+                        {createdRecipe.recipe.steps.map(step => (
+                            <li className='mt-2' key={step.step}>{step.step}</li>
+                        ))}
+                    </ol>
+                </div>
 
-        </>
+            </div>
+            <h1 className='font-bold sm:mb-0 mb-[4rem] sm:text-6xl text-5xl italic text-amber-800 dark:text-amber-600'>
+                Bon Apetite!
+            </h1>
+            <div className='fixed w-full px-5 bottom-5 flex sm:justify-between justify-center gap-5'>
+                {!addToRecipesBtn &&
+                    <ShareRecipeModal>
+                        <Button
+                            variant='secondary'
+                            className='min-w-[8rem] flex items-center gap-2 h-12 rounded-full px-5 hover:scale-105 transition duration-300 ease-in-out'
+                        >
+                            Share <Share2 className='size-4' />
+                        </Button>
+                    </ShareRecipeModal>
+                }
+                {addToRecipesBtn}
+            </div>
+        </main>
+
     )
 }
 
