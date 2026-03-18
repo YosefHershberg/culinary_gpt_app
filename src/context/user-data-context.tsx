dc723766094cd6624e6c78b71d083e2ee2c39010
Yosef Hershberg <berioo2000@gmail.com>
2026-03-18 09:57:04 +0200

    removed unneccessery isSignedIn check in UserDataProvider

--- src/context/user-data-context.tsx
+++ src/context/user-data-context.tsx
@@ -1,22 +1,12 @@
 import React from 'react'
 
 import { useSuspenseQueries } from '@tanstack/react-query'
-import { useAuth } from '@/context/auth-context'
 import { getUserIngredientsAPI } from '@/services/ingredient.service'
 import { getUserKitchenUtilsAPI } from '@/services/kitchenUtils.service'
 import { INGREDIENTS_QUERY_KEY, KITCHEN_UTILS_QUERY_KEY } from '@/lib/queryKeys'
 
+// NOTE: This provider is rendered only if user is signed in.
 export const UserDataProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { isSignedIn } = useAuth()
-
-  if (!isSignedIn) {
-    return <>{children}</>
-  }
-
-  return <UserDataFetcher>{children}</UserDataFetcher>
-}
-
-const UserDataFetcher: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   useSuspenseQueries({
     queries: [
       {
