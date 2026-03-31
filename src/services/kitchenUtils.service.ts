a9f0a83ac5ee2b4112a8bc9d9055981d86a40bcb
Yosef Hershberg <berioo2000@gmail.com>
2026-03-31 20:11:45 +0300

    fix kitchen util bug

--- src/services/kitchenUtils.service.ts
+++ src/services/kitchenUtils.service.ts
@@ -1,12 +1,32 @@
 import axiosClient from "@/config/axiosClient"
-import type { KitchenUtils } from "@/lib/types"
+import type { KitchenUtil, KitchenUtils } from "@/lib/types"
+
+const COLUMN_TO_DISPLAY: Record<string, KitchenUtil> = {
+    stoveTop: "Stove Top",
+    oven: "Oven",
+    microwave: "Microwave",
+    airFryer: "Air Fryer",
+    blender: "Blender",
+    foodProcessor: "Food Processor",
+    slowCooker: "Slow Cooker",
+    bbq: "BBQ",
+    grill: "Grill",
+}
 
 export const getUserKitchenUtilsAPI = async (): Promise<KitchenUtils> => {
     const { data } = await axiosClient.get('/user/kitchen-utils')
-    return data
+    return Object.entries(data).reduce((acc, [key, val]) => {
+        const displayName = COLUMN_TO_DISPLAY[key]
+        if (displayName) acc[displayName] = val as boolean
+        return acc
+    }, {} as KitchenUtils)
 }
 
 export const toggleUserKitchenUtilAPI = async (util: string): Promise<KitchenUtils> => {
-    const { data } = await axiosClient.patch('/user/kitchen-utils', { name: util, value: true })
-    return data
+    const { data } = await axiosClient.patch('/user/kitchen-utils', { name: util })
+    return Object.entries(data).reduce((acc, [key, val]) => {
+        const displayName = COLUMN_TO_DISPLAY[key]
+        if (displayName) acc[displayName] = val as boolean
+        return acc
+    }, {} as KitchenUtils)
 }
